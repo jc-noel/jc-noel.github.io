1c281f29d627aea4c21c1d2c6a531f0c489ca667
James Noel <110778112+jc-noel@users.noreply.github.com>
2024-05-22 14:01:08 -0500

    testing action to query imgur images

---
+++ assets/js/fetch-imgur-images.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const clientId = process.env.IMGUR_CLIENT_ID;
+const albumId = process.env.IMGUR_ALBUM_ID;
+const dataFilePath = path.join(__dirname, '_data', 'imgur-images.json');
+
+const fetchImgurImages = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.imgur.com/3/album/${albumId}/images`,
+      {
+        headers: { Authorization: `Client-ID ${clientId}` },
+      }
+    );
+
+    if (response.status === 200 && response.data.success) {
+      const images = response.data.data.map((image) => ({
+        id: image.id,
+        link: image.link,
+        width: image.width,
+        height: image.height,
+        datetime: image.datetime,
+      }));
+      const newData = JSON.stringify(images, null, 2);
+
+      let oldData = '';
+      if (fs.existsSync(dataFilePath)) {
+        oldData = fs.readFileSync(dataFilePath, 'utf8');
+      }
+
+      if (newData !== oldData) {
+        fs.writeFileSync(dataFilePath, newData);
+        console.log('Successfully updated imgur_images.json');
+      } else {
+        console.log('No changes detected in imgur_images.json');
+      }
+    } else {
+      console.error('Failed to fetch images:', response.status);
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error.message);
+  }
+};
+
+fetchImgurImages();
